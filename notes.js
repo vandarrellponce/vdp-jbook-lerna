775aac14bc45193dbd32228acce83d88e9d17802
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-18 18:54:28 +0800

    adding typescript support

--- notes.js
+++ notes.js
@@ -2,3 +2,11 @@
 - lerna add [module] --scope=[target] 
 - lerna add commander --scope=cli
 */
+
+/* Adding typscript 
+
+lerna add typescript --dev --scope=local-api
+
+and then initialize a tsconfig.json file by 
+npx typescript --init
+*/
