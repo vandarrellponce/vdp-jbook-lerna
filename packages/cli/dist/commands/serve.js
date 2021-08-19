e136ab70abb2a0a4900660ff0c0023d0719f726a
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-19 13:06:36 +0800

    specifying command line options

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -3,8 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 var commander_1 = require("commander");
 exports.serveCommand = new commander_1.Command()
-    .command('serve')
+    .command('serve [filename]') // [] brackets means that this is an optional value for commander
     .description('Open a file for editing')
-    .action(function () {
-    console.log('Getting ready to serve a file');
+    .option('-port, --port <number>', 'port to run the server on', '4005') // <> brackets means that is a required value for commander
+    .action(function (filename, options) {
+    if (filename === void 0) { filename = 'notebook.js'; }
+    // first arg is the optional value, next is an object with required value
+    console.log(filename, options);
 });
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,10 @@
 import { Command } from 'commander'
 
 export const serveCommand = new Command()
-  .command('serve')
+  .command('serve [filename]') // [] brackets means that this is an optional value for commander
   .description('Open a file for editing')
-  .action(() => {
-    console.log('Getting ready to serve a file')
+  .option('-port, --port <number>', 'port to run the server on', '4005') // <> brackets means that is a required value for commander
+  .action((filename = 'notebook.js', options) => {
+    // first arg is the optional value, next is an object with required value
+    console.log(filename, options)
   })
