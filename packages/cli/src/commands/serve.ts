371c99d923dc48ae2ea1f6a6978f6f525b5424a1
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-19 19:51:53 +0800

    wrapping express listen with a promise to handle async errors of express

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,9 +6,13 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-port, --port <number>', 'port to run the server on', '4005')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename))
-    const basename = path.basename(filename)
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename))
+      const basename = path.basename(filename)
 
-    serve(Number(options.port), basename, dir)
+      await serve(Number(options.port), basename, dir)
+    } catch (error) {
+      console.log(error.message)
+    }
   })
