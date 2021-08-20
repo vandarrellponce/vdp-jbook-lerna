57320811e2077def2340fe27a1b49d8dc3606bc3
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-20 15:55:07 +0800

    understanding package links

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,14 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
-var http_proxy_middleware_1 = require("http-proxy-middleware");
 var serve = function (port, filename, dir) {
     var app = express_1.default();
-    app.use(http_proxy_middleware_1.createProxyMiddleware({
+    app.use(express_1.default.static('../../local-client/build'));
+    /*  app.use(
+      createProxyMiddleware({
         target: 'http://localhost:3000',
         ws: true,
         logLevel: 'silent',
-    }));
+      })
+    ) */
     return new Promise(function (resolve, reject) {
         app
             .listen(port, function () {
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -4,13 +4,15 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express()
 
-  app.use(
+  app.use(express.static('../../local-client/build'))
+
+  /*  app.use(
     createProxyMiddleware({
       target: 'http://localhost:3000',
       ws: true,
       logLevel: 'silent',
     })
-  )
+  ) */
 
   return new Promise<void>((resolve, reject) => {
     app
