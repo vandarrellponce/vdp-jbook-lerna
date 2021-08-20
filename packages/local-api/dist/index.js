c861ddd3479b87f322b0084a7fa8a7883acdd0c8
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-20 17:59:14 +0800

    defining the path for the local-client

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
 var serve = function (port, filename, dir) {
     var app = express_1.default();
-    app.use(express_1.default.static('../../local-client/build'));
+    var packagePath = require.resolve('local-client/build/index.html');
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
     /*  app.use(
       createProxyMiddleware({
         target: 'http://localhost:3000',
