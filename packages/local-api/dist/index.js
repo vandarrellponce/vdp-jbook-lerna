c0cc3b84c5978c475cd9498225b954a715e3da25
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-23 13:21:20 +0800

    refractoring package names and linking again by running lerna bootstrap

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -20,7 +20,7 @@ var serve = function (port, filename, dir, useProxy) {
         }));
     }
     else {
-        var packagePath = require.resolve('local-client/build/index.html');
+        var packagePath = require.resolve('@vdp-cli/local-client/build/index.html');
         app.use(express_1.default.static(path_1.default.dirname(packagePath)));
     }
     return new Promise(function (resolve, reject) {
